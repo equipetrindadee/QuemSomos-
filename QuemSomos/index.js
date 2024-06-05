b48510d043af9cd046a910b019c8e7e965b5cec7
equipetrindadee <167995952+equipetrindadee@users.noreply.github.com>
2024-06-05 16:49:22 -0300

    Add files via upload

---
+++ QuemSomos/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../QuemSomos/QuemSomos.css';
+
+export const QuemSomos = () => {
+
+  return (
+    <div>
+        <body>
+            <h1 className="title-qs">Quem Somos?</h1>
+            <div className="conteudo-qs">
+                <div className="teste">
+                <img src="../../img/menininhos.png"  alt=""/> 
+                <p>Nós da equipe Alpha develop estamos empolgados para compartilhar o resultado de semanas de dedicação.  Cada membro traz uma peça única para o quebra-cabeça da tecnologia.  Deixe-me te dizer que estamos incrivelmente orgulhosos do que conseguimos alcançar.  Mas não precisa apenas confiar nessas palavras.</p>
+                </div>
+               
+            </div>
+        </body>
+    </div>
+  );
+}
+
+export default QuemSomos;
